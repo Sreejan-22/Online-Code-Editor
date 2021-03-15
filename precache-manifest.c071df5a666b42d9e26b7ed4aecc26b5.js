073def5d889c9faae00e9d24f36b9af2eab1ea4f
Sreejan Chaudhury <sreejan147@gmail.com>
2021-03-15 10:30:45 +0530

    Updates

---
+++ precache-manifest.c071df5a666b42d9e26b7ed4aecc26b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f63aaa9ed6570b8b2b64a17624bf9bb",
+    "url": "/online-code-editor/index.html"
+  },
+  {
+    "revision": "bf6ca47402fceb041b37",
+    "url": "/online-code-editor/static/css/2.6f92e09b.chunk.css"
+  },
+  {
+    "revision": "c87abb169688e6963a37",
+    "url": "/online-code-editor/static/css/main.7ea06a56.chunk.css"
+  },
+  {
+    "revision": "bf6ca47402fceb041b37",
+    "url": "/online-code-editor/static/js/2.fdc4f841.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "/online-code-editor/static/js/2.fdc4f841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87abb169688e6963a37",
+    "url": "/online-code-editor/static/js/main.7ff3a3ce.chunk.js"
+  },
+  {
+    "revision": "6c037b93c95f6c4d2cf5",
+    "url": "/online-code-editor/static/js/runtime-main.1a6b4da7.js"
+  }
+]);
